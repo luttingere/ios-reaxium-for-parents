b863a6b66c29ee6f09ff54503fe42cd061f51fdb
jrodrigueztera <jrodriguez@teravisiontech.com>
2016-06-01 07:30:07 -0400

    Pods update

---
+++ Pods/Target Support Files/Pods-ReaxiumForParentsTests/Pods-ReaxiumForParentsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReaxiumForParentsTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReaxiumForParentsTests
+@end
---
+++ Pods/Target Support Files/Pods-ReaxiumForParentsUITests/Pods-ReaxiumForParentsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReaxiumForParentsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ReaxiumForParentsUITests
+@end
