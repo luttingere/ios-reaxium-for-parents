e5b2640a227cb7b12ea7ec4f3c15a8257ceb1dcc
miguel <fvega@teravisiontech.com>
2017-09-28 20:01:21 -0300

    Added gitignore
    Ignored DC_Store & all pods

--- Pods/Target Support Files/KSToastView/KSToastView-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "KSToastView.h"
-
-FOUNDATION_EXPORT double KSToastViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char KSToastViewVersionString[];
-
--- Pods/Target Support Files/Pods-ReaxiumForParentsTests/Pods-ReaxiumForParentsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReaxiumForParentsTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReaxiumForParentsTests
-@end
